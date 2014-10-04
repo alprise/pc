ba0294807ccdcf02969b7b2ab2ceb9290ec52610
alprise <cristian.prisecaru@gmail.com>
2014-10-04 15:02:05 +0200

    updated

---
+++ src/AboutMe.Web.Mvc/AboutMe.Web.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AboutMe.Web.Mvc.Startup))]
+namespace AboutMe.Web.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
--- src/Pca.Web.Mvc/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Pca.Web.Mvc.MvcApplication" Language="C#" %>
